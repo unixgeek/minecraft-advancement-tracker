e058c005cedfe28c90b4a400a5b1da9205c19c6b
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2021-03-18 19:49:27 -0500

    Use a library to capitalize the names.

--- static/index.js
+++ static/index.js
@@ -1,3 +1,5 @@
+const titleCase = require("ap-style-title-case");
+
 import("../pkg").then(module => {
     const fileInput = document.getElementById("fileInput");
     fileInput.addEventListener("change", handleFile, false);
@@ -7,12 +9,10 @@ import("../pkg").then(module => {
 
         let file = this.files[0];
         file.text().then(text => {
-            // console.log(text);
-
             let missing_advancements = module.get_missing_advancements(text);
             let html = "";
             missing_advancements.forEach(advancement => {
-                html += "<h1>" + capitalize(advancement.name.replace("minecraft:", "").replace("/", ": ").replaceAll("_", " ")) + "</h1>";
+                html += "<h1>" + titleCase(advancement.name.replace("minecraft:", "").replace("/", ": ").replaceAll("_", " ")) + "</h1>";
                 html += "<ul>";
                 advancement.criteria.forEach(criteria => {
                     html += "<li>" + criteria.replace("minecraft:", "").replaceAll("_", " ") + "</li>";
@@ -24,15 +24,3 @@ import("../pkg").then(module => {
         });
     }
 }).catch(console.error);
-
-// Yeah yeah, too lazy to follow the "rules" and nobody cares about this app anyway.
-function capitalize(string) {
-    let capitalizedString = "";
-    string.split(" ").forEach(word => {
-        capitalizedString += word[0].toUpperCase();
-        capitalizedString += word.substring(1);
-        capitalizedString += " ";
-    })
-
-    return capitalizedString.trim();
-}
